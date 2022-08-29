eb2eb01c15cb1278e6c9b248026d99c4255548c2
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-08-29 13:51:16 +0200

    created sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -9,10 +10,12 @@ const SignIn = () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>signin</h1>
-      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 // you need to actually get a document instance. But when you want to access the data on those documents, you need to use get dock. And when you want to set the data, you need to set dock.
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -24,19 +25,26 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 // So Google Auth Provider is essentially a class that we get from Firebase Authentication and this is connected to Google auth itself.
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 // same one for all app only one auth
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 // And what this method is, is it's an async function that receives some user authentication object because that's really what we're getting back anyways from our Firebase authentication, our Google assignment.
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
   // databse, collection, uqnik id tell what it was
   const userDocRef = doc(db, "users", userAuth.uid);
 
@@ -54,6 +62,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        // Let's say we get some additional information. And this additional information I'm just going to say is an object. And what I'll do is I will spread this object in at the end after all of these fields have been filled from any previous variable setting that we've done. So if display name exists on user off, then it will get spread through and we're not going to pass any additional information, meaning that this will probably by default be just an empty object, for example. But let's say in our current example display name, there's no value here. It was null as we saw. So what's going to happen is display name is going to get set to null. But then because what we're going to do is we are going to add the additional information ourselves. Then what'll happen is that this is instead going to be passed in like this, where we will have a string ourselves. So it'll be Mike, for example, and then this is going to overwrite that null value so that we do actually have a final display name inside of our user document.
+        ...additionalInformation,
       });
     } catch (error) {
       console.log("error creating the user", error.message);
@@ -63,4 +73,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   // if user data exits
   // return userDocRef
   return userDocRef;
+};
+
+export const createAuthUserEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
